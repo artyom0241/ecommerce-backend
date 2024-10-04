d6b332d1aedc25a67f3569e9da19b9a521486d7e
artyom0241 <artem1756.m@gmail.com>
2024-10-05 00:49:18 +0300

    Bug fixed and tests completed

--- src/test/java/com/ecommercebackend/service/JWTServiceTest.java
+++ src/test/java/com/ecommercebackend/service/JWTServiceTest.java
@@ -1,11 +1,16 @@
 package com.ecommercebackend.service;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.ecommercebackend.model.LocalUser;
 import com.ecommercebackend.model.dao.LocalUserDAO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,6 +23,9 @@ public class JWTServiceTest {
 
     @Autowired
     private LocalUserDAO localUserDAO;
+
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
 
     @Test
     public void testVerificationTokenNotUsableForLogin() {
@@ -33,5 +41,20 @@ public class JWTServiceTest {
         String token = jwtService.generateJWT(user);
         Assertions.assertEquals(user.getUsername(), jwtService.getUsername(token),
                 "Token for auth should contain users username.");
+    }
+
+    @Test
+    public void testJWTNotGeneratedByUs() {
+        String token =
+                JWT.create().withClaim("USERNAME", "UserA").sign(Algorithm.HMAC256(
+                        "NotTheRealSecret"));
+        Assertions.assertThrows(SignatureVerificationException.class, () -> jwtService.getUsername(token));
+    }
+
+    @Test
+    public void testJWTCorrectlySignedNoIssuer() {
+        String token =
+                JWT.create().withClaim("USERNAME", "UserA").sign(Algorithm.HMAC256(algorithmKey));
+        Assertions.assertThrows(MissingClaimException.class, () -> jwtService.getUsername(token));
     }
 }
